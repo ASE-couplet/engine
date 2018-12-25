74cbdae292b21732e0bd1acee1e2fe1fc0b2057a
qsyao <870114339@qq.com>
2018-12-25 03:57:06 -0800

    kernel

---
+++ predict_poetry/kernel/web_test.py
@@ -0,0 +1,43 @@
+import argparse
+import sys
+import random
+import logging
+
+from plan import Planner
+from predict import Seq2SeqPredictor
+from match import MatchUtil
+
+import tensorflow as tf
+
+import ipdb
+
+class Main_Poetry_maker:
+    def __init__(self):
+        self.planner = Planner()
+        self.predictor = Seq2SeqPredictor()
+        self.Judge = MatchUtil()
+
+    def predict(self, input_ustr):
+        input_ustr = input_ustr.strip()
+        keywords = self.planner.plan(input_ustr)
+        lines = self.predictor.predict(keywords)
+        result = self.Judge.eval_rhyme(lines)
+        while(result == False):
+            lines = self.predictor.predict(keywords)
+            result = self.Judge.eval_rhyme(lines)
+        logging.debug(lines[0]+'('+keywords[0]+')' +'  '+lines[1]+'('+keywords[1]+')'+'  '+lines[2]+'('+keywords[2]+')'+'  '+lines[3]+'('+keywords[3]+')')
+        return lines[0] +'  '+lines[1] + '  '+lines[2]+'  '+lines[3]
+        
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--Input', type=str, 
+        help='input from users',
+        default='')
+
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    input_ustr = parse_arguments(sys.argv[1:]).Input
+    maker = Main_Poetry_maker()
+    ipdb.set_trace()
+    print(maker.predict(input_ustr))
---
+++ predict_poetry/test_engine.py
@@ -0,0 +1,59 @@
+import argparse
+import time
+import random
+import sys
+
+from sqlalchemy import create_engine, MetaData, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+
+import ipdb
+
+test_tags = ["雪，月", "风，花", "树，鸟", "雷，云"]
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--Mode', type=str, 
+        help='server or dev',
+        default='server')
+    return parser.parse_args(argv)
+
+if __name__ == "__main__":
+    mode = parse_arguments(sys.argv[1:]).Mode
+
+    if mode == "dev":
+        engine = create_engine('sqlite:///test_couplet.db?check_same_thread=False', echo=False)
+    else:
+        engine = create_engine("postgresql+psycopg2://poemscape:@poemscape.db", echo=True)
+    # # # Creating the table : ---------------
+    # from sqlalchemy import Column,Integer,String
+
+    # Base = declarative_base()
+
+    # class Order(Base):
+    #     __tablename__ = 'Order'
+
+    #     id = Column(Integer, primary_key=True)
+    #     image = Column(String)
+    #     tags = Column(String)
+    #     poems = Column(String)
+
+    # Base.metadata.create_all(engine)
+
+    # ipdb.set_trace() 
+
+    metadata = MetaData()
+    metadata.reflect(engine, only=['Order'])
+    Base = automap_base(metadata=metadata)
+    Base.prepare()
+    Order = Base.classes.Order
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+    for i in range(100):
+        randn = random.randint(100000,200000)
+        sess.add(Order(id=randn, image=str(randn) +".png", tags=test_tags[randn % 4]))  
+        sess.commit()
+        time.sleep(5)
+        item = sess.query(Order).filter_by(id=randn).first()
+        print(item.poems)
